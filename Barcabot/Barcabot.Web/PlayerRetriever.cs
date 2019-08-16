aa334381bcf361705cf8acaaacd2d98be9414ac5
Trace <lukaszdabrowski881@gmail.com>
2019-08-16 05:06:33 +0200

    removed flow through exception in ChartsModule and CardsModule.

--- Barcabot/Barcabot.Web/PlayerRetriever.cs
+++ Barcabot/Barcabot.Web/PlayerRetriever.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Barcabot.Common;
 using Barcabot.Common.DataModels;
