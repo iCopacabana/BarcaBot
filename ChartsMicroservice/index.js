30d06e06cb770e4278cc45dea1c74a51213c971c
Trace <lukaszdabrowski881@gmail.com>
2019-08-16 04:41:58 +0200

    addessed codacy js issues

--- ChartsMicroservice/index.js
+++ ChartsMicroservice/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.get('/', function (req, res) {
     const a = {
-        apiStatus: "working"
+        apiStatus: 'working'
     }
 
     res.send(a);
@@ -25,13 +25,13 @@ app.get('/', function (req, res) {
 app.post('/stats/player', function (req, res) {
     const stats = req.body.Per90Stats;
     const name = convertName(req.body.Name);
-    const x = ["Shots", "Shots on Target", "Key Passes", "Tackles", "Blocks", "Interceptions", "Duels Won", "Dribbles Attempted", "Dribbles Won", "Fouls Drawn", "Fouls Committed"];
+    const x = ['Shots', 'Shots on Target', 'Key Passes', 'Tackles', 'Blocks', 'Interceptions', 'Duels Won', 'Dribbles Attempted', 'Dribbles Won', 'Fouls Drawn', 'Fouls Committed'];
     const y = [stats.Shots.Total, stats.Shots.OnTarget, stats.Passes.KeyPasses, stats.Tackles.TotalTackles, stats.Tackles.Blocks, stats.Tackles.Interceptions, stats.Duels.Won, stats. Dribbles.Attempted, stats.Dribbles.Won, stats.Fouls.Drawn, stats.Fouls.Committed];
     const figure = chart.getOneTraceFigure(x, y, `${name} Per 90 Stats`);
    
     plotly.getImage(figure, imgOpts, function (error, imageStream) {
         if (error) {
-            res.status(500).send("error");
+            res.status(500).send('error');
             console.log(error)
         } else {
             imageStream.pipe(res);
@@ -44,7 +44,7 @@ app.post('/stats/players', function(req, res) {
     const stats2 = req.body.PlayerList[1].Per90Stats;
     const name1 = convertName(req.body.PlayerList[0].Name);
     const name2 = convertName(req.body.PlayerList[1].Name);
-    const x = ["Shots", "Shots on Target", "Key Passes", "Tackles", "Blocks", "Interceptions", "Duels Won", "Dribbles Attempted", "Dribbles Won", "Fouls Drawn", "Fouls Committed"];
+    const x = ['Shots', 'Shots on Target', 'Key Passes', 'Tackles', 'Blocks', 'Interceptions', 'Duels Won', 'Dribbles Attempted', 'Dribbles Won', 'Fouls Drawn', 'Fouls Committed'];
     const y1 = [stats1.Shots.Total, stats1.Shots.OnTarget, stats1.Passes.KeyPasses, stats1.Tackles.TotalTackles, stats1.Tackles.Blocks, stats1.Tackles.Interceptions, stats1.Duels.Won, stats1.Dribbles.Attempted, stats1.Dribbles.Won, stats1.Fouls.Drawn, stats1.Fouls.Committed];
     const y2 = [stats2.Shots.Total, stats2.Shots.OnTarget, stats2.Passes.KeyPasses, stats2.Tackles.TotalTackles, stats2.Tackles.Blocks, stats2.Tackles.Interceptions, stats2.Duels.Won, stats2.Dribbles.Attempted, stats2.Dribbles.Won, stats2.Fouls.Drawn, stats2.Fouls.Committed];
 
@@ -53,7 +53,7 @@ app.post('/stats/players', function(req, res) {
    
     plotly.getImage(figure, imgOpts, function (error, imageStream) {
         if (error) {
-            res.status(500).send("error");
+            res.status(500).send('error');
             console.log(error)
         } else {
             imageStream.pipe(res);
