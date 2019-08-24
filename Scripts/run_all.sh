8f2e8b4c6d51312acd6005d60f65186fe7fe0f76
Trace <lukaszdabrowski881@gmail.com>
2019-08-24 09:27:38 +0200

    removed old charts script from run_all

--- Scripts/run_all.sh
+++ Scripts/run_all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 nohup ./run_img_microservice.sh >/dev/null 2>&1 & disown
-nohup ./run_charts_microservice.sh >/dev/null 2>&1 & disown
 nohup ./run_hangfire_microservice.sh >/dev/null 2>&1 & disown
 nohup ./run_bot.sh >/dev/null 2>&1 & disown
 
